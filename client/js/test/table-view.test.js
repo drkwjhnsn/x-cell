1b402ec106791b960a1a8826ed0e04e851a7f9e3
Derek Johnson <drk.w.jhnsn@gmail.com>
2017-04-27 11:06:32 -0500

    update and focus formula bar

--- client/js/test/table-view.test.js
+++ client/js/test/table-view.test.js
@@ -12,7 +12,46 @@ describe('table-view', () => {
 
   });
 
+  describe('formula-bar', () => {
+    it('updates FROM the value of the current cell', () => {
+      // set up initial state
+      const model = new TableModel(3,3);
+      const view = new TableView(model);
+      model._setValue({col: 2, row: 1}, '123');
+      view.init();
+
+      // inspect initial state
+      const formulaBarEl = document.querySelector('#formula-bar');
+      expect(formulaBarEl.value).toBe('');
+      // simulate user action
+      const trs = document.querySelectorAll('TBODY TR');
+      trs[1].cells[2].click();
+      // inspect resulting state
+      expect(formulaBarEl.value).toBe('123');
+    })
+  });
+
   describe('table-body', () => {
+    it('highlights the current cell when clicked', () => {
+      // set up the initial state
+      const model = new TableModel(10,5);
+      const view = new TableView(model);
+      view.init();
+
+      // inspect the initial state
+      let trs = document.querySelectorAll('TBODY TR');
+      let td = trs[2].cells[3];
+      expect(td.className).toBe('');
+
+      // simulate user action
+      td.click();
+
+      // inspect the resulting state
+      trs = document.querySelectorAll('TBODY TR');
+      td = trs[2].cells[3];
+      expect(td.className).not.toBe('');
+    })
+
     it('has the right size', () => {
       // set up initial state
       const numCols = 6;
